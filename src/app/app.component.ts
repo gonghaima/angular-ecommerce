7bc40f82f4dabc0ec77d2a6502aa3ab4d2b4662b
Steven Gong <gonghaima@hotmail.com>
2025-03-29 09:22:30 +1100

    import componet to use

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  template: `
-    <h1>Welcome to {{title}}!</h1>
+  imports: [RouterOutlet, HeaderComponent],
+  template: `<app-header/>`,
+  // template: `
+  //   <h1>Welcome to {{title}}!</h1>
 
-    <router-outlet />
-  `,
+  //   <router-outlet />
+  // `,
   styles: [],
 })
 export class AppComponent {
