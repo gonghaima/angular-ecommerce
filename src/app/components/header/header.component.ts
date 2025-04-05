6fbe27f9245781bdd767439a74308bcc82227efd
Steven Gong <gonghaima@hotmail.com>
2025-04-06 08:49:40 +1000

    use signal pass value into the child component

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -7,7 +7,7 @@ import { PrimaryButtonComponent } from '../primary-button/primary-button.compone
   template: `
     <div class="bg-slate-100 px-4 py-3 shadow-md flex justify-between">
       <span>My Store</span>
-      <app-primary-button label="Cart" (btnClicked)="showButtonClicked()" />
+      <app-primary-button [label]="cart()" (btnClicked)="showButtonClicked()" />
     </div>
   `,
   styles: `
@@ -16,6 +16,7 @@ import { PrimaryButtonComponent } from '../primary-button/primary-button.compone
 })
 export class HeaderComponent {
   title = signal('My Ecommerce App');
+  cart = signal('Cart');
   showButtonClicked() {
     console.log('clicked');
   }
