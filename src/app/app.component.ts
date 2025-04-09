bb3b299ecd421ff1b64db9124b830b4024c70314
Steven Gong <gonghaima@hotmail.com>
2025-04-10 07:09:09 +1000

    signal for product list & display it in html、

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
+import { ProductsListComponent } from './pages/product-list/product-list.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent],
-  template: `<app-header/>`,
+  imports: [RouterOutlet, HeaderComponent, ProductsListComponent],
+  template: `
+    <app-header />
+    <app-product-list />
+  `,
   // template: `
   //   <h1>Welcome to {{title}}!</h1>
 
