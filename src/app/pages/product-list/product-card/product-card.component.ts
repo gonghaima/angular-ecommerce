b7a1ff5852f8d47e4b509ad20b10f04dc7ff875a
Steven Gong <gonghaima@hotmail.com>
2025-04-18 09:35:15 +1000

    add app-primary-button

--- src/app/pages/product-list/product-card/product-card.component.ts
+++ src/app/pages/product-list/product-card/product-card.component.ts
@@ -1,9 +1,10 @@
 import { Component, input } from '@angular/core';
 import { Product } from '../../../models/products.model';
+import { PrimaryButtonComponent } from '../../../components/primary-button/primary-button.component';
 
 @Component({
   selector: 'app-product-card',
-  imports: [],
+  imports: [PrimaryButtonComponent],
   template: `
     <div
       class="bg-white shadow-md border rounded-xl p-6 flex flex-col gap-6 relative"
@@ -14,13 +15,14 @@ import { Product } from '../../../models/products.model';
           class="w-[200px] h-[100px] object-contain"
         />
       </div>
-      <div class="flex flex-col">
+      <div class="flex flex-col mt-2">
         <span class="text-md font-bold">
           {{ product().title }}
         </span>
         <span class="text-md font-bold">
           {{ '$' + product().price }}
         </span>
+        <app-primary-button label="Add to Cart" class="mt-3" />
       </div>
     </div>
   `,
