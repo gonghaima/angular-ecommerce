d70549195158d31a5a34b6f87f3a19deffdc6034
Steven Gong <gonghaima@hotmail.com>
2025-04-28 19:24:55 +1000

    cart-item show info

--- src/app/pages/cart/cart-item/cart-item.component.ts
+++ src/app/pages/cart/cart-item/cart-item.component.ts
@@ -4,7 +4,19 @@ import { Product } from '../../../models/products.model';
 @Component({
   selector: 'app-cart-item',
   imports: [],
-  template: ` <p>cart-item works!</p> `,
+  template: `
+    <div
+      class="bg-white shadow-md border rounded-xl p-6 flex gap-4 items-center"
+    >
+      <img [src]="item().image" class="w-[50px] h-[50px] object-contain" />
+      <div class="flex flex-col">
+        <span class="text-md font-bold">{{ item().title }}</span>
+        <span class="text-sm"> {{ '$' + item().price }}</span>
+      </div>
+      <div class="flex-1"></div>
+      app-button
+    </div>
+  `,
   styles: ``,
 })
 export class CartItemComponent {
